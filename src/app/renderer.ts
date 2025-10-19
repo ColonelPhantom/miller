99ae7f9b07e9e7a76e194bb099dc3d749e642828
Quinten Kock <quinten@quinten.space>
2025-10-20 01:48:05 +0200

    Move all logic to TS; remove all HTML other than <body>

--- src/app/renderer.ts
+++ src/app/renderer.ts
@@ -10,8 +10,24 @@ import { FolderTreeView } from "./foldernav";
 import { EditorGrid, addEditor } from "./editorgrid";
 import * as u from "./utils";
 
-van.add(document.querySelector("aside nav"), FolderTreeView);
-van.add(document.getElementById("editorGrid"), EditorGrid);
+const app = v.div(
+    { class: "h-screen max-h-screen flex flex-col" },
+    v.header(
+        { class: "flex-none" },
+        v.button({ id: "addEditor", onclick: addEditor }, "Add Editor"),
+    ),
+    v.div(
+        { id: "content", class: "flex flex-1 min-h-0" },
+        v.aside(
+            {
+                class: "flex-none resize-x overflow-x-hidden overflow-y-scroll w-3xs min-w-32",
+            },
+            FolderTreeView,
+        ),
+        EditorGrid,
+    ),
+);
 
-document.getElementById("addEditor")?.addEventListener("click", addEditor);
+van.add(document.body, app);
+
 addEditor();

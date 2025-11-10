0dd9ddc5b47d998829f49d31826e91047dfaff38
Quinten Kock <quinten@quinten.space>
2025-11-10 03:37:28 +0100

    Simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,15 @@
-import { defineConfig } from "eslint/config";
-import { fixupConfigRules } from "@eslint/compat";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-});
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
+    globalIgnores([".vite/"]),
     {
-        extends: fixupConfigRules(
-            compat.extends(
-                "eslint:recommended",
-                "plugin:@typescript-eslint/eslint-recommended",
-                "plugin:@typescript-eslint/recommended",
-                "plugin:import/recommended",
-                "plugin:import/electron",
-                "plugin:import/typescript",
-            ),
-        ),
-
-        ignores: [".vite/**"],
-
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-
-            parser: tsParser,
-        },
+        // files: ["src/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
     },
+    tseslint.configs.recommended,
 ]);
